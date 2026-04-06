7c0c5d2573365da056910fe4b3e6efa89ab56965
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-04-06 10:44:14 +0100

    Email and password form data is showing in  console but Lottie react is not working

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,27 +1,49 @@
+// import Lottie from "lottie-react";
 import { NavLink } from "react-router";
-import registerAnimation from "../../assets/RegisterLottie.json";
-import Lottie from "lottie-react";
+// import registerLottie from "../../assets/register.json";
 
 const Register = () => {
+
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(email, password);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left"></div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <Lottie animationData={registerAnimation} loop={true}></Lottie>
+          {/* <Lottie animationData={registerLottie} loop={true}></Lottie> */}
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <form className="fieldset">
+            {/* Form of Registration */}
+
+            <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
               <div>
                 <NavLink to="/login" className="link link-hover">
                   Already Have an account ?
                 </NavLink>
               </div>
-              <button className="btn btn-neutral mt-4">Register</button>
+              <button type="submit"  className="btn btn-neutral mt-4">Register</button>
             </form>
           </div>
         </div>

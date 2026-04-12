604f6c662fac56beee42d0738c4216e555e7094f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-04-12 23:56:34 +0100

    Handler Function and SIgnin change is done

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -1,16 +1,31 @@
+// import Lottie from "lottie-react";
 import { NavLink } from "react-router";
+
+
 const SignIn = () => {
+  
+
+  const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(email, password);
+
+    
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left"></div>
-        <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           {/* <Lottie animationData={registerLottie} loop={true}></Lottie> */}
           <div className="card-body">
-            <h1 className="text-5xl font-bold">Sign In</h1>
+            <h1 className="text-5xl font-bold">SignIN now!</h1>
             {/* Form of Registration */}
 
-            <form  className="fieldset">
+            <form onSubmit={handleSignIn} className="fieldset">
               <label className="label">Email</label>
               <input
                 name="email"
@@ -27,11 +42,11 @@ const SignIn = () => {
               />
               <div>
                 <NavLink to="/login" className="link link-hover">
-                 Doesn't have an account ?
+                Doesn't have an account ?
                 </NavLink>
               </div>
               <button type="submit" className="btn btn-neutral mt-4">
-                SignIn
+                Sign In
               </button>
             </form>
           </div>

0f37bf8c53cfedd01be54aca00ec0eab3ff48d96
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-04-29 06:03:23 +0100

    In Bannner image is not showing and Framer motion also used

--- src/Components/Home/Banner.tsx
+++ src/Components/Home/Banner.tsx
@@ -1,31 +1,16 @@
-import React from 'react';
-import {motion} from 'motion/react';
-
-
+import React from "react";
+import { motion } from "motion/react";
 
 const Banner = () => {
-    return (
-       <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    <div>
-      
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-      <motion.h1
-      animate={{rotate:360,transition:{duration:2}}}
-      
-      >Hi Its my first animation</motion.h1>
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <motion.img
+          animate={{scale: 2, transition:{duration : 2}}}
+          src="../../assets/Job Board.jpg" alt="" />
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;

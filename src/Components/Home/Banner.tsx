bbbc5163fc557ec2613d02141b70fc413e09e20a
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-05-03 00:29:49 +0100

    Banner Picture is added, It is seen successfully on UI

--- src/Components/Home/Banner.tsx
+++ src/Components/Home/Banner.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
+
+
 
 const Banner = () => {
+
   return (
+
     <div className="hero bg-base-200 min-h-screen">
+      
       <div className="hero-content flex-col lg:flex-row">
-        <motion.img
+      
+      
+        {/* <img
           animate={{scale: 2, transition:{duration : 2}}}
-          src="../../assets/Job Board.jpg" alt="" />
+          src="../../assets/Job Board.jpg" alt="" /> */}
+
+          {/* Banner Image  */}
+
+          <img src="/public/picture/Job.jpg" alt="" />
+
+          <h1>Hi This h1 tag</h1>
+
+
+          {/* <motion.h1
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            How are you my friend?
+          </motion.h1> */}
+
+
       </div>
     </div>
   );

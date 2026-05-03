eae12ecda1be58a71a9bd3e323062f4e7b0e7709
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-05-03 13:01:00 +0100

    Image and framer motion are working perfectly

--- src/Components/Home/Banner.tsx
+++ src/Components/Home/Banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { motion } from "motion/react";
+import { motion } from "motion/react";
 
 
 
@@ -12,25 +12,12 @@ const Banner = () => {
       <div className="hero-content flex-col lg:flex-row">
       
       
-        {/* <img
-          animate={{scale: 2, transition:{duration : 2}}}
-          src="../../assets/Job Board.jpg" alt="" /> */}
+        
 
-          {/* Banner Image  */}
-
-          <img src="/public/picture/Job.jpg" alt="" />
-
-          <h1>Hi This h1 tag</h1>
-
-
-          {/* <motion.h1
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.5 }}
-          >
-            How are you my friend?
-          </motion.h1> */}
-
+          <motion.img
+          animate = {{rotate:360   , transition:{duration:3}}}
+          
+          src="/public/picture/Job.jpg" alt="" />
 
       </div>
     </div>

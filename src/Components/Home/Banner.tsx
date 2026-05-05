b837144ddc8ab562b00b1df5792892251329f2a0
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-05-05 21:21:12 +0100

    React motion or framer motion is integrated properly to the projects

--- src/Components/Home/Banner.tsx
+++ src/Components/Home/Banner.tsx
@@ -8,35 +8,35 @@ const Banner = () => {
 
         <div className="absolute top-0 left-0">
           <div className="flex">
-
             <div className="w-2/5">
               <p className="text-black">
                 The Easiest Way to Find Your{" "}
                 <span className="text-purple-500">First</span> Dream Job
               </p>
-              <p  className="text-black">
+              <p className="text-black">
                 Each month, more than 3 million job seekers turn to website in
                 their search for work, making over 140,000 applications every
                 single day.
               </p>
             </div>
 
+            <div className="w-3/5 flex mt-20 gap-4">
+              <motion.img
+                animate={{ y: [0, -30, 0] }}
+                transition={{ duration: 2, repeat: Infinity }}
+                className="w-10 flex-1 border-black border-4 border-dashed  "
+                src="/public/picture/team1.jpg"
+                alt=""
+              />
 
-
-
-            <div className="w-3/5 flex">
-              <motion.img 
-              animate={{rotate:360, repeatCount: Infinity, duration: 2}}
-              className="w-10 flex-1" src="/public/picture/team1.jpg" alt="" />
-
-
-              <img
-                className="w-10 h-min flex-1"
+              <motion.img
+                animate={{ x: [0, 30, 0] }}
+                transition={{ duration: 2, repeat: Infinity }}
+                className="w-5 h-min flex-1  border-black border-4 border-dashed"
                 src="/public/picture/team3.jpg"
                 alt=""
               />
             </div>
-
           </div>
         </div>
       </div>
